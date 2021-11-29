384c8be9d2c99a900caef260b4d4a7e39ac8a96b
Jonathan Zavala <jontzavala@gmail.com>
2021-11-29 15:14:45 -0600

    commit tree ee8ccdc162d0c39e46b09f4f0f54e53923a6eb27

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,20 @@
+import uuid from 'uuid';
+
 export default function manageBand(state = {
   bands: [],
 }, action) {
   switch(action.type) {
     case "ADD_BAND":
-      return {...state, bands: state.bands.concat(action.name)}
+      const band = {
+        id: uuid(),
+        name: action.name
+      }
+
+      return { bands: state.bands.concat(band) };
+
+    case 'DELETE_BAND':
+      const bands= state.bands.filter(bands => bands.id !== action.id)
+      return {bands}
     default:
       return state
   
